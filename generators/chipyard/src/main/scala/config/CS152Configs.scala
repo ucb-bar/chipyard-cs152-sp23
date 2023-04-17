bea645dcb0612c94c2368ce27d029e9de70c84ae
Jamie Hong <jamiehong@berkeley.edu>
2023-04-17 14:48:31 -0700

    Update lab5 rocket configs

--- generators/chipyard/src/main/scala/config/CS152Configs.scala
+++ generators/chipyard/src/main/scala/config/CS152Configs.scala
@@ -1,9 +1,15 @@
 package chipyard
 
-import freechips.rocketchip.config.{Config}
+import chisel3._
+
+import freechips.rocketchip.config.{Config, Parameters}
 import freechips.rocketchip.rocket._
 import freechips.rocketchip.subsystem._
 import freechips.rocketchip.tile.{RocketTileParams}
+
+/**********************************************************************
+ * Lab 5
+ **********************************************************************/
 
 class WithNLab5Cores(n: Int) extends Config((site, here, up) => {
   case RocketTilesKey => {
@@ -16,27 +22,30 @@ class WithNLab5Cores(n: Int) extends Config((site, here, up) => {
         rowBits = site(SystemBusKey).beatBits,
         nSets = 16,
         nWays = 4,
-        nTLBEntries = 4,
+        nTLBSets =  1,
+        nTLBWays = 4,
         nMSHRs = 0,
         blockBytes = site(CacheBlockBytes))),
       icache = Some(ICacheParams(
         rowBits = site(SystemBusKey).beatBits,
         nSets = 64,
         nWays = 4,
-        nTLBEntries = 4,
+        nTLBSets = 1,
+        nTLBWays = 4,
         blockBytes = site(CacheBlockBytes))))
     List.tabulate(n)(i => lab5.copy(hartId = i))
   }
 })
 
+
 class Lab5RocketConfig extends Config(
-  new chipyard.iobinders.WithUARTAdapter ++                      // display UART with a SimUARTAdapter
-  new chipyard.iobinders.WithTieOffInterrupts ++                 // tie off top-level interrupts
-  new chipyard.iobinders.WithBlackBoxSimMem ++                   // drive the master AXI4 memory with a SimAXIMem
-  new chipyard.iobinders.WithTiedOffDebug ++                     // tie off debug (since we are using SimSerial for testing)
-  new chipyard.iobinders.WithSimSerial ++                        // drive TSI with SimSerial for testing
-  new testchipip.WithTSI ++                                      // use testchipip serial offchip link
-  new chipyard.config.WithNoGPIO ++                              // no top-level GPIO pins (overrides default set in sifive-blocks)
+  new chipyard.harness.WithUARTAdapter ++                      // display UART with a SimUARTAdapter
+  new chipyard.harness.WithTieOffInterrupts ++                 // tie off top-level interrupts
+  new chipyard.harness.WithBlackBoxSimMem ++                   // drive the master AXI4 memory with a SimAXIMem
+  new chipyard.harness.WithTiedOffDebug ++                     // tie off debug (since we are using SimSerial for testing)
+  new chipyard.harness.WithSimSerial ++                        // drive TSI with SimSerial for testing
+  // new testchipip.WithTSI ++                                      // use testchipip serial offchip link
+  // new chipyard.config.WithNoGPIO ++                              // no top-level GPIO pins (overrides default set in sifive-blocks)
   new chipyard.config.WithBootROM ++                             // use default bootrom
   new chipyard.config.WithUART ++                                // add a UART
   new freechips.rocketchip.subsystem.WithNoMMIOPort ++           // no top-level MMIO master port (overrides default set in rocketchip)
